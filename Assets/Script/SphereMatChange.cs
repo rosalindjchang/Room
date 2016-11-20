6e8e96dbbbb4de54c73012a9da8a53f2c410a0ca
Rosalind Chang <rosalindjchang@gmail.com>
2016-11-19 22:29:42 -0800

    room light

---
+++ Assets/Script/SphereMatChange.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereMatChange : MonoBehaviour {
+
+	private Renderer rend;
+
+	void Start () {
+		rend = GetComponent <Renderer> ();
+	}
+	
+	void Update () {
+	
+		if (Input.GetMouseButtonDown (0)) {
+			rend.material.SetColor ("_Color", Color.red);
+		}
+	}
+}
