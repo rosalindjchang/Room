56a7e23fc2a5cc93a9c25e6d239b0084bbe399c3
Rosalind Chang <rosalindjchang@gmail.com>
2016-11-30 10:37:48 -0800

    added start screen

---
+++ Assets/Script/StartPlaying.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class StartPlaying : MonoBehaviour {
+
+	public void Update () {
+
+		if (Input.GetMouseButtonDown (0)) {
+			SceneManager.LoadScene ("Play");
+			SceneManager.UnloadScene ("Start");
+
+		}
+	}
+}
